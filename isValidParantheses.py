d6eb5e43fb31c241628ce5a3a44935767eed51ae
Chinmay <chinmaypanghri@gmail.com>
2024-11-14 21:59:21 +0530

    Update isValidParantheses.py
    refactored it to make it usable (lol i just made the input string a parameter instead of creating it within the function)

--- isValidParantheses.py
+++ isValidParantheses.py
@@ -1,24 +1,23 @@
-def isValidParantheses()-> bool:
+def isValidParantheses(p: str)-> bool:
 
     valid_pars = {   #dictionary for valid pairs of brackets
     ')':'(',
     '}':'{',
     ']':'['
-    }
-
-    p = input("Enter the expression\n")   #accepting the input string
+    }   
   
     stack = []   #stack used for checking
 
-    for par in p:   #iterate through each character in the input string
-        if par in valid_pars.values():  #checks if par is opening bracket
-            stack.append(par)           #push par to stack if its an opening bracket
-        elif par in valid_pars:         #checks if its a closing bracket
-            if len(stack) and stack[-1]==valid_pars[par]: 
+    for char in p:   #iterate through each character in the input string
+        if char in valid_pars.values():  #checks if par is opening bracket
+            stack.append(char)           #push par to stack if its an opening bracket
+        elif char in valid_pars:         #checks if its a closing bracket
+            if len(stack) and stack[-1]==valid_pars[char]: 
                 stack.pop()        #pops if it finds a matching opening bracket
             else:
                 return False       #false if string has trailing closing brackets or if it doesn't find a matching opening bracket
     
     return len(stack)==0           #returns whether stack is empty at last
 
-print(isValidParantheses())
+p = input("Enter the expression\n");
+print("The parantheses are valid" if isValidParantheses(p) else "The parantheses are not written correctly; try to correct it")
